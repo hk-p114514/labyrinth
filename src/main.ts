651d6226587b75dbc4d48cdaa0163489cde637e2
hk-p114514 <hukada.gogotea@gmail.com>
2021-07-21 13:45:57 +0900

    とりあえずリロードできるようになった

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,6 @@ const playerColor: string = "red";
 const goalColor: string = "green";
 
 // - システム変数の定義
-
 const game = {
   clear: false,
   time: 0,
@@ -29,5 +28,26 @@ const game = {
   goalY: col - 2,
 };
 
-startGame(row, col, canvas, blockSize, rateOfIncrease);
+let maze = new Maze(row, col, new MazeRenderer(canvas, blockSize, row, col));
+
+// - 迷路を描画し、迷路自身を取得する
+let data: number[][] = maze.render();
+
+let player = new Player(1, 1, data);
+
+document.addEventListener("keydown", (event: KeyboardEvent) => {
+  if (!game.clear) {
+    player.move(event);
+    console.log(`is not clear : ${game.clear}`);
+  } else {
+    console.log(`CLEAR! : ${game.clear}`);
+    game.clear = false;
+    maze = new Maze(row, col, new MazeRenderer(canvas, blockSize, row, col));
+    player = new Player(1, 1, data);
+    if (ctx) {
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    data = maze.render();
+  }
+});
 export { canvas, ctx, playerColor, goalColor, game, blockSize };
